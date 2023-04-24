9f1afc3e3afd8f37a22633be3a0341433cda2ef0
Jorge Ochoa Reyes <106556358+jorgeochoareyes2@users.noreply.github.com>
2023-04-23 21:18:07 -0700

    Fixes for local build

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
 import type { ExpoConfig, ConfigContext } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_aW5mb3JtZWQtYm94ZXItNjUuY2xlcmsuYWNjb3VudHMuZGV2JA";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
